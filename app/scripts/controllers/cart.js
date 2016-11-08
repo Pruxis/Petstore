1ed66204b500e3aa1c99b8b525f9de5a3751f1ae
Pruxis <llavaert@gmail.com>
2016-11-08 11:52:13 +0100

    Generating routes

---
+++ app/scripts/controllers/cart.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name petstoreApp.controller:CartCtrl
+ * @description
+ * # CartCtrl
+ * Controller of the petstoreApp
+ */
+angular.module('petstoreApp')
+  .controller('CartCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/store.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name petstoreApp.controller:StoreCtrl
+ * @description
+ * # StoreCtrl
+ * Controller of the petstoreApp
+ */
+angular.module('petstoreApp')
+  .controller('StoreCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
