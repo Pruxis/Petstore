a63bc7289b4dfba9f1dabf364b5889d9c57b3b09
Pruxis <llavaert@gmail.com>
2016-11-08 11:51:05 +0100

    Generating file structure

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name petstoreApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the petstoreApp
+ */
+angular.module('petstoreApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name petstoreApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the petstoreApp
+ */
+angular.module('petstoreApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
